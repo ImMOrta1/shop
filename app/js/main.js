8ee4475b85d0d20850c00a9336e4a0c237f3c171
ImM0rta1 <webart@bk.ru>
2015-10-27 23:14:15 +0300

    alpha-build 1

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,24 @@ $(document).ready(function() {
 
 		var $this = $(this),
 			item = $this.closest('.accordion__item'),
-			hideItem = item.find('.accordion__item-container'),
-			arrow = $this.closest('.accordion__arrow')
+			hide = item.find('.accordion__item-container'),
+			arrow = item.find('.accordion__arrow')
 			duration = 600;
 
-		hideItem.stop(true, true).slideToggle(duration);
+		if(!item.hasClass('close')) {
+
+			hide.stop(true, true).slideUp(duration);
+			item.addClass('close');
+			arrow.removeClass('accordion__arrow_up');
+			arrow.addClass('accordion__arrow_down');
+
+		} else {
+
+			hide.stop(true, true).slideDown(duration);
+			item.removeClass('close');
+			arrow.removeClass('accordion__arrow_down');
+			arrow.addClass('accordion__arrow_up');
+		}
 
 	});
 
@@ -19,13 +32,31 @@ $(document).ready(function() {
 
 		var $this = $(this),
 			item = $this.closest('.accordion__item-container');
-			
-			item.find('input').removeProp('checked');
+			checkboxes = item.find('input:checkbox');
+
+			checkboxes.prop("checked", false);
 
 	});
-	// colunnizer
 
+	// price-slider
+	$(function() {
+		
+    	$('.price-slider__linebar-hover').slider({
+      		range: true,
+      		min: 0,
+      		max: 50000,
+      		values: [ 0, 50000 ],
+    		slide: function( event, ui ) {
+    			$('.price-val__start').find('.price-val__input')
+    			.val($('.price-slider__linebar-hover').slider( "values", 0 ));
 
+    			$('.price-val__over').find('.price-val__input')
+    			.val($('.price-slider__linebar-hover').slider( "values", 1 ));	
+      		}
+    	});	
+  	});
+
+	// columnizer
 	var columnizer = function() {
 
 		$('.information').columnize({
